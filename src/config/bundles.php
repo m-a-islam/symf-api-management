86ff66745903f2ba6dd3565358d644cf1439f9d0
muhammad <muhammad.cse36@gmail.com>
2025-08-21 09:40:05 +0200

    feat: logout success

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
